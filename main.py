09de2045154b5e1444e2d00d94e2ad7b0bcb219c
SantiPe <santiagopereira0812@gmial.com>
2023-11-30 19:58:10 -0300

    avance

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 from jmetal.algorithm.singleobjective.genetic_algorithm import GeneticAlgorithm
-from jmetal.operator import BinaryTournamentSelection, SPXCrossover, BitFlipMutation
+
+from jmetal.operator import BestSolutionSelection, RandomSolutionSelection
+from jmetal.operator import IntegerPolynomialMutation
+from jmetal.operator import CXCrossover,DifferentialEvolutionCrossover,NullCrossover,PMXCrossover,SBXCrossover,SPXCrossover
+
+
 from jmetal.util.termination_criterion import StoppingByEvaluations
 from src.models.DietProblem import DietProblem
 from src.utils.utils import generate_food_array
 
 def main():
-    food_array = generate_food_array('data/comidas.csv')
+    food_array = generate_food_array('data/iguales.csv')
     food_ids = [food['id'] for food in food_array]
 
     problem = DietProblem(number_of_meals=4, number_of_days=7, number_of_objectives=1, food_ids=food_ids, food_objects=food_array)
 
     algorithm = GeneticAlgorithm(
         problem=problem,
-        population_size=100,
-        offspring_population_size=50,
-        mutation=BitFlipMutation(probability=0.01),
-        crossover=SPXCrossover(probability=0.9),
-        selection=BinaryTournamentSelection(),
-        termination_criterion=StoppingByEvaluations(max_evaluations=25000)
+        population_size=4,
+        offspring_population_size=2,
+        mutation=IntegerPolynomialMutation(probability=0.1),
+        crossover=CXCrossover(probability=0.0),
+        selection=RandomSolutionSelection(),
+        termination_criterion=StoppingByEvaluations(max_evaluations=30)
     )
 
     algorithm.run()
     result = algorithm.get_result()
 
-    print(f"Fitness: {result.objectives[0]}")
+    #print(result.variables)
     print(f"Solution: {result.variables}")
+    print(f"Fitness: {result.objectives[0]}")
 
     return result
 
--- src/models/DietProblem.py
+++ src/models/DietProblem.py
@@ -1,10 +1,12 @@
 import numpy as np
 import re
-from jmetal.core.problem import FloatProblem
-from jmetal.core.solution import FloatSolution
+#from jmetal.core.problem import FloatProblem
+#from jmetal.core.solution import FloatSolution
+from jmetal.core.problem import IntegerProblem
+from jmetal.core.solution import IntegerSolution
 from src.models.Config import Config
 
-class DietProblem(FloatProblem):
+class DietProblem(IntegerProblem):
 
     def __init__(self, number_of_meals: int, number_of_days: int, number_of_objectives: int,
                  food_ids: int, food_objects: list):
@@ -19,42 +21,58 @@ class DietProblem(FloatProblem):
         self.obj_directions = [self.MINIMIZE]
         self.obj_labels = ['Fitness']
 
-        self.lower_bound = [0.0 for _ in range(number_of_meals * number_of_days)]
-        self.upper_bound = [1.0 for _ in range(number_of_meals * number_of_days)]
-        
+        self.lower_bound = [1 for _ in range(number_of_meals * number_of_days)]
+        self.upper_bound = [len(food_ids) for _ in range(number_of_meals * number_of_days)]
+
         # Cargar la configuración
         self.config = Config('config\config.ini')
         
-    def evaluate(self, solution: FloatSolution) -> FloatSolution:
+    def evaluate(self, solution: IntegerSolution) -> IntegerSolution:
         total_fitness = 0
-
-        for i in range(self.number_of_days*self.number_of_meals): 
-            c = solution.variables[i]  # comida elegida en un horario
-
-            # Buscar la comida en el array de comidas
-            meal = self.food_objects[c - 1]
-
-            if meal is None:
-                raise ValueError(f"No se encontró la comida con id {c}")
-
-            # Calcula la función de fitness para cada comida
-            fitness = self.config.alpha * abs(self.config.kc - meal['kcal']) + \
-                    self.config.beta * (abs(self.config.p - meal['p']) + abs(self.config.hc - meal['hc']) + abs(self.config.g - meal['g'])) + \
-                    self.config.gamma * self.pond_horario(c, i % 4 ) + \
-                    self.config.sigma * self.cant_rep(c, solution)
-
-            total_fitness += fitness ** 2
-
+        #print('solution.variables: ' + str(solution.variables))
+        for day in range(self.number_of_days): 
+            fitness_day = 0
+            kcal_acc = 0
+            p_acc = 0
+            hc_acc = 0
+            g_acc = 0
+            for food in range(self.number_of_meals):
+                
+                c = solution.variables[day * self.number_of_meals + food]  # comida elegida en un horario
+                
+                # Buscar la comida en el array de comidas
+                meal = self.food_objects[c - 1]
+            
+                if meal is None:
+                    raise ValueError(f"No se encontró la comida con id {c}")
+                
+                #acumular los valores de cada comida en un dia
+                kcal_acc += meal['kcal']    
+                p_acc += meal['p']
+                hc_acc += meal['hc']
+                g_acc += meal['g'] 
+                
+            fitness_day = self.config.alpha * abs(self.config.kc - kcal_acc) + \
+                self.config.beta * (abs(self.config.p - p_acc) + abs(self.config.hc - hc_acc) + abs(self.config.g - g_acc)) + \
+                self.config.gamma * self.pond_horario(c, food) + \
+                self.config.sigma * self.cant_rep(c, solution)
+            #print('fitness_day: ' + str(fitness_day))
+            #total_fitness += fitness_day# santiago
+            total_fitness += fitness_day ** 2
+        
+        #print('fitness_semanal: ' + str(total_fitness/self.number_of_days))
+        
         solution.objectives[0] = total_fitness ** 0.5
+        print('total_fitness: ' + str(total_fitness ** 0.5))
         return solution
 
-    def create_solution(self) -> FloatSolution:
-        new_solution = FloatSolution(lower_bound=self.lower_bound, upper_bound=self.upper_bound, number_of_objectives=self.number_of_objectives)
+    def create_solution(self) -> IntegerSolution:
+        new_solution = IntegerSolution(lower_bound=self.lower_bound, upper_bound=self.upper_bound, number_of_objectives=self.number_of_objectives)
         # Creao un array con 7x4=28 celdas donde cada uno representa una comida
-        #print('foods_ids', self.food_ids)
         for i in range(self.number_of_days*self.number_of_meals):
-                new_solution.variables[i] = np.random.choice(self.food_ids)
-        print(new_solution.variables)
+            new_solution.variables[i] = np.random.choice(self.food_ids)
+        #print('Nueva solución: ')
+        #print(new_solution.variables)
         return new_solution
 
     def pond_horario(self, c, h):
@@ -74,7 +92,8 @@ class DietProblem(FloatProblem):
     
     def name(self):
         return 'DietProblem'
-
+    
+        
     @property
     def number_of_constraints(self):
         return self._number_of_constraints
