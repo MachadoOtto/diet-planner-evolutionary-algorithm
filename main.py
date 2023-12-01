5d2bd6fd517498a8c4adcbcb9e960a2287b7a18b
SantiPe <santiagopereira0812@gmial.com>
2023-11-30 21:04:55 -0300

    nuevos crossover

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from jmetal.operator import CXCrossover,DifferentialEvolutionCrossover,NullCross
 from jmetal.util.termination_criterion import StoppingByEvaluations
 from src.models.DietProblem import DietProblem
 from src.utils.utils import generate_food_array
+from src.models.Config import Config
+from src.models.CrossoverCustom import ColumnCrossover, RowCrossover
 
 def main():
     food_array = generate_food_array('data/iguales.csv')
@@ -15,14 +17,17 @@ def main():
 
     problem = DietProblem(number_of_meals=4, number_of_days=7, number_of_objectives=1, food_ids=food_ids, food_objects=food_array)
 
+    config = Config('config\config.ini')
+    
+
     algorithm = GeneticAlgorithm(
         problem=problem,
-        population_size=4,
+        population_size=40,
         offspring_population_size=2,
         mutation=IntegerPolynomialMutation(probability=0.1),
-        crossover=CXCrossover(probability=0.0),
+        crossover=ColumnCrossover(probability=0.3, probabilityColumn=config.probabilityColumn, number_of_columns=problem.number_of_meals, number_of_rows=problem.number_of_days),
         selection=RandomSolutionSelection(),
-        termination_criterion=StoppingByEvaluations(max_evaluations=30)
+        termination_criterion=StoppingByEvaluations(max_evaluations=5000)
     )
 
     algorithm.run()
